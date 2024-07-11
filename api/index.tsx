bdb732b5a1cded394d17de17054e8bbc38f88c2d
juan <jpredcoin@gmail.com>
2024-07-11 11:09:40 -0500

    listo desplegado en vercel inthebackground

--- api/index.tsx
+++ api/index.tsx
@@ -12,7 +12,7 @@ import type { Address } from 'viem';
 // }
 
 export const app = new Frog({
-  title:'Bye sun',
+  title:'In the background',
   assetsPath: '/',
   basePath: '/api',
   // Supply a Hub to enable frame verification.
@@ -22,7 +22,7 @@ export const app = new Frog({
 app.frame('/', (c) => {
   return c.res({
     action: "/finish",
-    image: "https://gateway.lighthouse.storage/ipfs/bafkreifds6a7adz7trerslsaenwwx5bh3z32pxx2jdx5jrjc3ezxwsou6i",
+    image: "https://gateway.lighthouse.storage/ipfs/bafkreifwsrcrhfpwpmupupy47yiolpsg3ml6s2z7vwg6gg5rltniccft7q",
     intents: [
       
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
@@ -43,7 +43,7 @@ app.transaction('/mint', (c) => {
   const address = c.address as Address;
   console.log('address', address);
   const tokenId = 0;
-  const uri = 'https://gateway.lighthouse.storage/ipfs/bafkreihl42kpwk7sxili63ewzw6ogddg3eweg5v24u33fqgtbqy3m7rohu'; // Fixed URI
+  const uri = 'https://gateway.lighthouse.storage/ipfs/bafkreiapcgyj57rruhwyqhwkf3ox3l4xs5xw3rppabpgwamqvrj5pf7tuu'; // Fixed URI
 
   console.log('address', address);
   console.log('tokenId', tokenId);
@@ -56,7 +56,7 @@ app.transaction('/mint', (c) => {
     chainId: 'eip155:42161', // arbitrum one
     functionName: 'safeMint',
     args: [address, uri],
-    to: '0x8deEC033385F78f6135A909D1A3fc6a8a2e56576' // arbitrum one 0xE4D2ce5f8fd8c17A9aC3955B01dAdF50d5EECe48
+    to: '0x81B1354Cafc84d77D2d74ac609087fE56ac7E580' // arbitrum one 0xE4D2ce5f8fd8c17A9aC3955B01dAdF50d5EECe48
   });
 });
 
